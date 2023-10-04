1b7952dccf3bafa71958f2c46790d11de075442e
Mustfa1122 <145039363+Mustfa1122@users.noreply.github.com>
2023-10-04 00:12:07 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+
+var message = "Hello World"
+var userName = prompt('Enter Your First Name');
+var userName_a = prompt (' Enter Your Last Name');
+var num = prompt('Enter Your Mobile Number');
+var myName = (userName + userName_a);
+var age = "27 years old"
+var course = "Web and App Development"
+var msg = "PIZZA \nPIZZ \nPIZ \nPI \nP"
+var email = "My Email Address Is Example@gmail.com"
+var book =  "I am trying to learn from the book A \nsmarter way to learn JavaScript."
+var symbol = "“▬▬▬▬▬▬ஜ۩۩ஜ▬▬▬▬▬▬”"
+
+
+
+
+alert(message)
+document.write(myName)
+document.write(num)
+console.table(userName, userName_a, num);
+alert(myName)
+alert(age)
+alert(course)
+alert(msg)
+alert(email)
+alert(book)
+alert(symbol)
